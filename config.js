45c76f5144eaff64662a56f7f7f96fc7f9fab425
installcatalog <tanvi.mittal80@gmail.com>
2026-06-13 20:27:13 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  APP_NAME: "SaatPake",
+  TAGLINE: "Where Hearts Meet",
+
+  PRIMARY_COLOR: "#8B1E3F",
+  ACCENT_COLOR: "#D4AF37",
+
+  API_URL: "YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+
+  ADMIN_WHATSAPP: "91XXXXXXXXXX",
+
+  HOME_PAGE: "home.html",
+  LOGIN_PAGE: "index.html",
+
+  DEFAULT_IMAGE: "https://via.placeholder.com/400x500?text=SaatPake"
+};
